519ef3e2d6ecdf7439c71d4bba4689c74aa3371a
DarkDomian <107422893+DarkDomian@users.noreply.github.com>
2022-09-15 21:56:25 +0300

    just next step of my cod

--- BotWithDB.py
+++ BotWithDB.py
@@ -1,24 +1,34 @@
-from numbers import Real
 import sqlite3
 import telebot
 from datetime import date
-import random
 
 bot = telebot.TeleBot("5217938630:AAEdkkqc4aGpedu4FRfvmnx9DKgiMIlMJfI")
 
 conn = sqlite3.connect('db/database.db', check_same_thread=False)
 cursor = conn.cursor()
 
-def db_table_val(user_id: int, date: date, task: str, check: Real):
+def DataBaseVal(user_id: int, date: date, task: str, check: bool):
 	cursor.execute('INSERT INTO test (user_id, date, task, check) VALUES (?, ?, ?, ?)', (user_id, date, task, check))
 	conn.commit()
 
 HELP = """
 help : display a list of available commands and ther function,
-add : add a task to the list,
+add : add a task to the list, writing in format: "/add dd.mm.yyyy task text",
 show :  display all tasks,
-random : crate random task for that day,
+mark : chose complited task, and mark them,
 clear : make your chat purely\n"""
+
+@bot.message_handler(commands=["add"])
+def add(message):
+    UserID = message.from_user.id
+    SplitMessage = message.text.split(maxsplit=2)
+    DateFromMess = date.strptime(SplitMessage[1], "%d.%m.%y")  #need use date-format
+    TaskFromMess = SplitMessage[2]
+    DataBaseVal(user_id=UserID, date=DateFromMess, task=TaskFromMess, check=False)
+
+@bot.message_handler(commands=["show"])
+def show(message):
+    if message.from_user.id in 
 
 @bot.message_handler(commands=["help"])
 def help(message):
@@ -29,5 +39,9 @@ def clear(message):
     bot.delete_message(message.chat.id, message.message_id)
     bot.send_delete
 
+
+@bot.message_handler(content_types=['text', 'audio', 'file', 'image'])
+def text(message):
+    bot.send_message(message.chat.id, "It's beautiful, but what can I say?" )
 
 bot.polling(non_stop=True)
